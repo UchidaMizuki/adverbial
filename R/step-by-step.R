e6d4a9e31d5448b864bde9ca9bd93cd256c133ef
Uchida Mizuki <uchidamizuki@vivaldi.net>
2025-04-27 00:24:47 +0900

    Update `step_by_step()`

--- R/step-by-step.R
+++ R/step-by-step.R
@@ -1,52 +1,140 @@
-#' Wrap functions for step-by-step data processing
+new_step_by_step <- function(
+  object,
+  steps,
+  current_step,
+  ...,
+  class = character()
+) {
+  structure(
+    object,
+    adverbial_object_step_by_step = list(
+      steps = steps,
+      current_step = current_step
+    ),
+    ...,
+    class = c(
+      class,
+      "adverbial_object_step_by_step",
+      setdiff(class(object), "adverbial_object_step_by_step")
+    )
+  )
+}
+
+#' Create a step-by-step object
 #'
 #' `r lifecycle::badge("experimental")`
 #'
 #' @description
-#' `step_by_step()` creates a step-by-step object that allows you to apply
-#' functions to data in a sequence.
+#' `step_by_step()` creates a step-by-step object that can be used to track the
+#' progress of a process.
+#' It is useful for long-running processes where you want to keep track of
+#' the steps that have been completed and the steps that are still to be done.
 #'
-#' @param fns A list of functions to be applied step by step.
-#' @param descriptions A character vector of step descriptions.
-#' @param ... Additional arguments for attributes.
+#' @param steps A named vector of steps to be completed. The names of the
+#' vector are the names of the steps, and the values are the descriptions of
+#' the steps.
 #'
-#' @return A function that takes `data` and returns an object that inherits from
-#' `adverbial_object_step_by_step`.
+#' @return A function that takes an object and returns a step-by-step object.
 #'
 #' @export
-step_by_step <- function(fns, descriptions = NULL, ...) {
-  steps <- get_steps(
-    fns = fns,
-    descriptions = descriptions
-  )
-  steps$state <- "todo"
-  steps$state[[1]] <- "doing"
+step_by_step <- function(steps) {
+  steps <- vctrs::vec_cast(steps, character())
+  if (!rlang::is_named(steps)) {
+    cli::cli_abort("{.arg steps} must be a named vector.")
+  }
 
-  function(data) {
-    structure(
-      list(
-        steps = steps,
-        data = data
-      ),
-      class = "adverbial_object_step_by_step",
+  function(object, ...) {
+    new_step_by_step(
+      object,
+      steps = steps,
+      current_step = 0,
       ...
     )
   }
 }
 
-get_steps <- function(fns, descriptions) {
-  descriptions <- descriptions %||% purrr::map_chr(fns, pillar::obj_sum)
-  vctrs::data_frame(
-    step = names(fns),
-    fn = fns,
-    description = descriptions
-  )
+#' Wrap a function to be used as a step
+#'
+#' `r lifecycle::badge("experimental")`
+#'
+#' @description
+#' `wrap_step()` wraps a function to be used as a step in a step-by-step
+#' process.
+#'
+#' @param f A function to be wrapped.
+#' @param name The name of the step. If `NULL`, the step does not proceed but
+#' the function is applied.
+#'
+#' @return A function that takes a step-by-step object and additional
+#' arguments, and returns the updated step-by-step object.
+#'
+#' @export
+wrap_step <- function(f, name = NULL) {
+  name <- vctrs::vec_cast(name, character())
+
+  function(object, ...) {
+    next_step(f, name)(object, ...)
+  }
+}
+
+#' End a step-by-step process
+#'
+#' `r lifecycle::badge("experimental")`
+#'
+#' @description
+#' `end_step()` ends the step-by-step process and removes the step-by-step
+#' attributes from the object.
+#'
+#' @param object The object to end the step-by-step process for.
+#'
+#' @return The object with the step-by-step attributes removed.
+#'
+#' @export
+end_step <- function(object) {
+  attr(object, "adverbial_object_step_by_step") <- NULL
+  class(object) <- setdiff(class(object), "adverbial_object_step_by_step")
+  object
+}
+
+next_step <- function(f, name) {
+  function(object, ...) {
+    if (inherits(object, "adverbial_object_step_by_step")) {
+      attr_object <- attr(object, "adverbial_object_step_by_step")
+      steps <- attr_object$steps
+      current_step <- attr_object$current_step
+
+      if (is.null(name)) {
+        new_step_by_step(
+          f(object, ...),
+          steps = steps,
+          current_step = current_step
+        )
+      } else {
+        if (current_step == vctrs::vec_size(steps)) {
+          cli::cli_abort("No more steps to do.")
+        }
+
+        next_step_name <- names(steps)[[current_step + 1]]
+        if (name != next_step_name) {
+          cli::cli_abort("Next step must be {.fn {next_step_name}}.")
+        }
+
+        new_step_by_step(
+          f(object, ...),
+          steps = steps,
+          current_step = current_step + 1
+        )
+      }
+    } else {
+      f(object, ...)
+    }
+  }
 }
 
 #' @export
 print.adverbial_object_step_by_step <- function(x, ...) {
   print_steps(x)
-  print_step_data(x)
+  print(end_step(x))
 }
 
 print_steps <- function(x) {
@@ -56,53 +144,45 @@ print_steps <- function(x) {
   cat_line_subtle("#")
 }
 
-print_steps_state <- function(x) {
-  symbol_state <- c(
-    todo = cli::symbol$checkbox_off,
-    doing = cli::symbol$checkbox_off,
-    done = cli::symbol$checkbox_on
-  )
-  steps <- paste0(
-    vctrs::vec_seq_along(x$steps),
-    ". ",
-    x$steps$step
-  )
-  descriptions <- x$steps$description %||%
-    purrr::map_chr(x$steps$fn, pillar::obj_sum)
+print_steps_state <- function(object) {
+  attr_object <- attr(object, "adverbial_object_step_by_step")
+  steps <- attr_object$steps
+  current_step <- attr_object$current_step
 
+  size_steps <- vctrs::vec_size(steps)
+  seq_along_steps <- vctrs::vec_seq_along(steps)
+
+  symbol_state <- vctrs::vec_rep(cli::symbol$checkbox_off, size_steps)
+  symbol_state[1:current_step] <- cli::symbol$checkbox_on
+
+  descriptions <- unname(steps)
+  steps <- paste0(symbol_state, " ", seq_along_steps, ". ", names(steps), ": ")
   cat_line_subtle(
     "# ",
-    pillar::align(paste0(
-      symbol_state[x$steps$state],
-      " ",
-      steps,
-      ": "
-    )),
-    pillar::align(descriptions)
+    pillar::align(steps),
+    descriptions
   )
 }
 
-print_steps_info <- function(x) {
-  loc_next_step <- vctrs::vec_match("doing", x$steps$state)
-  if (is.na(loc_next_step)) {
+print_steps_info <- function(object) {
+  attr_object <- attr(object, "adverbial_object_step_by_step")
+  steps <- attr_object$steps
+  current_step <- attr_object$current_step
+
+  if (current_step == vctrs::vec_size(steps)) {
     cat_line_subtle(
       "# ",
       cli::symbol$info,
-      ' All steps are done. Please call `purrr::chuck("data")` to get the data.'
+      " All steps are done. Please call `end_step()`."
     )
   } else {
-    next_step <- encodeString(x$steps$step[[loc_next_step]], quote = "\"")
+    next_step <- names(steps)[[current_step + 1]]
     cat_line_subtle(
       "# ",
       cli::symbol$info,
-      " Please call `next_step(.step = ",
+      " Please call `",
       next_step,
-      ")` to continue."
+      "()` to continue."
     )
   }
-}
-
-print_step_data <- function(x) {
-  cat_line_subtle("# Data:")
-  print(x$data)
 }
--- tests/testthat/test-step-by-step.R
+++ tests/testthat/test-step-by-step.R
@@ -1,110 +1,42 @@
-fns <- list(
-  square = function(x) x^2,
-  sum = sum,
-  sqrt = sqrt
-)
-
 test_that("step-by-step works", {
+  fns <- list(
+    square = function(x) x^2,
+    sum = sum,
+    sqrt = sqrt
+  )
   cdist <- new_composed(fns, dir = "forward")
-  dist_calculator <- step_by_step(fns)
+
+  dist_calculator <- step_by_step(c(
+    square_step = "Square the input",
+    sum_step = "Sum the squares",
+    sqrt_step = "Take the square root"
+  ))
+  square_step <- wrap_step(function(x) x^2, "square_step")
+  sum_step <- wrap_step(sum, "sum_step")
+  sqrt_step <- wrap_step(sqrt, "sqrt_step")
 
   x <- c(1:10, NA)
   dist <- dist_calculator(x)
-  dist <- next_step(dist, "square")
-  dist <- next_step(dist, "sum")
-  dist <- next_step(dist, "sqrt")
+  dist <- square_step(dist)
+  dist <- sum_step(dist)
+  dist <- sqrt_step(dist)
+  dist <- end_step(dist)
 
-  expect_equal(dist$data, cdist(x))
-  expect_equal(dist$steps$state, rep("done", vctrs::vec_size(dist$steps)))
+  expect_equal(dist, cdist(x))
 
   fns_na_rm <- fns
   fns$sum <- new_partialised(sum, list(na.rm = TRUE))
   cdist_na_rm <- new_composed(fns, dir = "forward")
 
   dist <- dist_calculator(x)
-  dist <- next_step(dist, "square")
-  dist <- next_step(dist, "sum", na.rm = TRUE)
-  dist <- next_step(dist, "sqrt")
+  dist <- square_step(dist)
+  dist <- sum_step(dist, na.rm = TRUE)
+  dist <- sqrt_step(dist)
+  dist <- end_step(dist)
 
-  expect_equal(dist$data, cdist_na_rm(x))
-  expect_equal(dist$steps$state, rep("done", vctrs::vec_size(dist$steps)))
-})
+  expect_equal(dist, cdist_na_rm(x))
 
-test_that("next_step() works", {
-  dist_calculator <- step_by_step(fns)
-
-  x <- c(1:10, NA)
   dist <- dist_calculator(x)
-  expect_error(next_step(dist, "square_wrong"))
-
-  dist <- next_step(dist, "square")
-  expect_error(next_step(dist, "square"))
-})
-
-test_that("insert_step() works", {
-  dist_calculator <- step_by_step(
-    fns,
-    descriptions = c(
-      "Square the input",
-      "Sum the squares",
-      "Take the square root"
-    )
-  )
-
-  x <- c(1:10, NA)
-  dist <- dist_calculator(x)
-
-  dist_inserted <- insert_step(
-    dist,
-    list(sum2 = sum, sum3 = sum),
-    descriptions = c("Sum 2", "Sum 3"),
-    after = 2
-  )
-
-  expect_equal(dist_inserted$steps$step[3:4], c("sum2", "sum3"))
-  expect_equal(dist_inserted$steps$description[3:4], c("Sum 2", "Sum 3"))
-
-  expect_error({
-    dist_inserted <- next_step(dist, "square")
-    insert_step(dist_inserted, list(sum2 = sum, sum3 = sum), .after = 2)
-  })
-})
-
-
-test_that("update_step() works", {
-  dist_calculator <- step_by_step(
-    fns,
-    descriptions = c(
-      "Square the input",
-      "Sum the squares",
-      "Take the square root"
-    )
-  )
-
-  x <- c(1:10, NA)
-  dist <- dist_calculator(x)
-
-  dist_updated <- update_step(dist, 2, prod, description = "Product of squares")
-  expect_equal(dist_updated$steps$fn[[2]], prod)
-  expect_equal(dist_updated$steps$description[[2]], "Product of squares")
-
-  expect_error({
-    dist_updated <- next_step(dist, "square")
-    update_step(dist_updated, 2, prod, description = "Product of squares")
-  })
-})
-
-test_that("delete_step() works", {
-  dist_calculator <- step_by_step(fns)
-
-  x <- c(1:10, NA)
-  dist <- dist_calculator(x)
-
-  dist_deleted <- delete_step(dist, 2)
-  expect_equal(dist_deleted$steps$step[[2]], "sqrt")
-
-  expect_error({
-    dist_deleted <- next_step(dist, "square")
-    delete_step(dist_deleted, 2)
-  })
+  expect_error(sum_step(dist))
+  expect_error(sqrt_step(dist))
 })
